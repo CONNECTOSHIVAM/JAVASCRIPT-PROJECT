7952f3c8e1577323d982a31690e37cee24f778c0
connectoshivam <connecttoshivam1@gmail.com>
2026-02-14 09:52:47 +0530

    add the color generator functionlaties

--- JSP/colorCrunch/script.js
+++ JSP/colorCrunch/script.js
@@ -8,3 +8,13 @@ const buttons = {
 
 const box = document.querySelector('.box');
 const h2 = document.querySelector('h2');
+
+const generateColor = ()=>{
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for(let i=0;i<6;i++)
+    {
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color
+}
